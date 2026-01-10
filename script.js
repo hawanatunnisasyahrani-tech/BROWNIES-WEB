2135b1a49eda96f4de3cd9737ead714ce8b36d1a
hawanatunnisasyahrani-tech <hawanatunnisasyahrani@gmail.com>
2026-01-10 16:48:20 +0700

    Update script.js

--- script.js
+++ script.js
@@ -1,2 +1,21 @@
+function toggleMenu(){
+  document.getElementById("menu").classList.toggle("active");
+}
+
+// HERO SLIDER
+function initHeroSlider(){
+  const slides = document.querySelectorAll(".hero-slider img");
+  let index = 0;
+
+  function showSlide(i){
+    slides.forEach(s => s.classList.remove("active"));
+    slides[i].classList.add("active");
+  }
+
+  setInterval(()=>{
+    index = (index + 1) % slides.length;
+    showSlide(index);
+  },4000);
+}
 
 
